260b6883a2eacdee565cfe38689ea11d5d5cae47
agazadeilkin191-cmd <agazadeilkin191@gmail.com>
2026-04-19 16:52:42 +0300

    Update 12-roman_to_int.py

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-Roman to Integer module
+Module to convert Roman numeral to integer
 """
 
 def roman_to_int(roman_string):
@@ -10,16 +10,20 @@ def roman_to_int(roman_string):
     if not isinstance(roman_string, str) or roman_string is None:
         return 0
 
-    rd = {
+    roman_map = {
         'I': 1, 'V': 5, 'X': 10,
         'L': 50,
         'C': 100, 'D': 500, 'M': 1000
         }
-    num = 0
-    for i in range(len(roman_string)):
-        curr = rd.get(roman_string[i], 0)
-        if i + 1 < len(roman_string) and rd.get(roman_string[i + 1], 0) > curr:
+    total = 0
+    prev_value = 0
+    
+    for char in reversed(roman_string):
+        value = roman_map.get(char, 0)
+        if value < prev_value
             num -= current
         else:
-            num += current
+            total -= value
+            prev_value = value
+            
     return num
