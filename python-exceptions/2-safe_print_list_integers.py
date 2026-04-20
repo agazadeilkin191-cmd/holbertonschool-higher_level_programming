e12b33ec4f5c9ac9ed1ac380154a58b7237129d9
Ilkin Agazade <agazadeilkin191@email.com>
2026-04-20 15:06:05 +0000

    Add task 2: safe_print_list_integers

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -3,14 +3,21 @@
 
 
 def safe_print_list_integers(my_list=[], x=0):
-    """Prints the first x elements of a list that are integers."""
+    """Prints the first x elements of a list that are integers.
 
+    Args:
+        my_list (list): The list to process.
+        x (int): The number of elements to check.
+
+    Returns:
+        int: The number of integers successfully printed.
+    """
     count = 0
     for i in range(x):
         try:
             print("{:d}".format(my_list[i]), end="")
             count += 1
-        except (ValueError, TypeError)
+        except (ValueError, TypeError):
             continue
-print()
-return count
+    print()
+    return count
