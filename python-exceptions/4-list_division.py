90fd933bba2bf93ca96c6fda4dae25425abada47
Ilkin Agazade <agazadeilkin191@email.com>
2026-04-20 16:05:25 +0000

    Add list_division function

--- python-exceptions/4-list_division.py
+++ python-exceptions/4-list_division.py
@@ -8,7 +8,7 @@ def list_division(my_list_1, my_list_2, list_length):
                 print("out of range")
                 div = 0
             elif not isinstance(my_list_1[i], (int, float)) or
-                   not isinstance(my_list_2[i], (int, float)):
+                   not isinstance(my_list_2[i], (int, float))):
                 print("wrong type")
                 div = 0
             else:
