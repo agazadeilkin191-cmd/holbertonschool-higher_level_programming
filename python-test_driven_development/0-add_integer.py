a1dc7d636895c9faf3cdccd04092b396be9a5a9c
Ilkin Agazade <agazadeilkin191@email.com>
2026-04-21 17:22:50 +0000

    Fix: Handle NaN and overflow in add_integer

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -10,8 +10,10 @@ def add_integer(a, b=98):
     Raises:
         TypeError: If a or b is not an integer or float.
     """
-    if not isinstance(a, (int, float)):
+    if not isinstance(a, (int, float))
+       or (isinstance(a, float) and a != a):
         raise TypeError("a must be an integer")
-    if not isinstance(b, (int, float)):
+    if not isinstance(b, (int, float))
+       or (isinstance(b, float) and b != b):
         raise TypeError("b must be an integer")
     return int(a) + int(b)
