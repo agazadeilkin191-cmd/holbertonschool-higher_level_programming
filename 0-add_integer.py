8301bc7903015d8cdab249033d460b417ddf76a0
Ilkin Agazade <agazadeilkin191@email.com>
2026-04-21 18:04:33 +0000

    Add execution permissions

--- 0-add_integer.py
+++ 0-add_integer.py
@@ -1,9 +1,9 @@
 def add_integer(a, b=98):
-    if type(a) not in [int, float]:
+    if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
-    if type(b) not in [int, float]:
+    if not isinstance(b, (int, float)):
         raise TypeError("b must be an integer")
- 
+
     if isinstance(a, float) and (a != a or a == float('inf') or a == float('-inf')):
         raise TypeError("a must be an integer")
     if isinstance(b, float) and (b != b or b == float('inf') or b == float('-inf')):
