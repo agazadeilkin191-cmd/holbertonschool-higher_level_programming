44cbf78006f0efe08ac3631921a0729b0fd5bbae
Ilkin Agazade <agazadeilkin191@email.com>
2026-04-21 18:30:48 +0000

    Fix float overflow

--- 0-add_integer.py
+++ 0-add_integer.py
@@ -1,32 +1,11 @@
 #!/usr/bin/python3
-"""Defines an integer addition function."""
-
-
 def add_integer(a, b=98):
-    """Adds two integers.
-
-    Args:
-        a: The first integer or float.
-        b: The second integer or float, defaults to 98.
-
-    Returns:
-        The sum of a and b as an integer.
-    """
     if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
     if not isinstance(b, (int, float)):
         raise TypeError("b must be an integer")
-
-    # NaN (Not a Number) check
-    if isinstance(a, float) and (a != a):
+    if isinstance(a, float) and (a != a or a == float('inf') or a == float('-inf')):
         raise TypeError("a must be an integer")
-    if isinstance(b, float) and (b != b):
+    if isinstance(b, float) and (b != b or b == float('inf') or b == float('-inf')):
         raise TypeError("b must be an integer")
-
-    # Infinity check (prevents float overflow)
-    if isinstance(a, float) and (a == float('inf') or a == float('-inf')):
-        raise TypeError("a must be an integer")
-    if isinstance(b, float) and (b == float('inf') or b == float('-inf')):
-        raise TypeError("b must be an integer")
-
     return int(a) + int(b)
