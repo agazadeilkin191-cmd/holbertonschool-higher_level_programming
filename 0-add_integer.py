1323ce2207f2e518aaccc1246cb33c47bcdcd4f2
Ilkin Agazade <agazadeilkin191@email.com>
2026-04-21 17:56:16 +0000

    Fix: Final clean version

--- 0-add_integer.py
+++ 0-add_integer.py
@@ -1,12 +1,12 @@
 def add_integer(a, b=98):
-    if not isinstance(a, (int, float)):
+    if type(a) not in [int, float]:
         raise TypeError("a must be an integer")
-    if not isinstance(b, (int, float)):
+    if type(b) not in [int, float]:
         raise TypeError("b must be an integer")
-    
+ 
     if isinstance(a, float) and (a != a or a == float('inf') or a == float('-inf')):
         raise TypeError("a must be an integer")
     if isinstance(b, float) and (b != b or b == float('inf') or b == float('-inf')):
         raise TypeError("b must be an integer")
-        
+
     return int(a) + int(b)
