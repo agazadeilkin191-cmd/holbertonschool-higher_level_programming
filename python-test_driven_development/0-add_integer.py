f33c0a5667d8c7703366beaf846204835b53416c
Ilkin Agazade <agazadeilkin191@email.com>
2026-04-22 15:55:48 +0000

    Fixing docstring and types

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,11 +1,15 @@
 #!/usr/bin/python3
 """
-Module that provides the add_integer function.
+Module 0-add_integer
+Contains a function that adds two integers
 """
 
 
 def add_integer(a, b=98):
-    """Adds two integers or floats and returns the result as an integer."""
+    """Adds two integers or floats.
+
+    Returns an integer: the sum of a and b
+    """
     if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
     if not isinstance(b, (int, float)):
