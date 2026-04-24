4bc29760d7de3c33373f6219be38489488aabe19
Ilkin Agazade <agazadeilkin191@email.com>
2026-04-24 15:20:18 +0000

    Fix final tests

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 """
-Module 3-say_my_name
+3-say_my_name modulu
 """
 
 
 def say_my_name(first_name, last_name=""):
     """
-    Function that prints My name is <first name> <last name>
+    first_name və last_name çap edən funksiya
     """
     if not isinstance(first_name, str):
         raise TypeError("first_name must be a string")
