f2f1e5804e8c908dbcfb3dcd47b8850d4ae6f4b8
Ilkin Agazade <agazadeilkin191@email.com>
2026-04-24 15:35:38 +0000

    Add task 4-print_square with tests

---
+++ python-test_driven_development/4-print_square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+Module 4-print_square
+"""
+
+
+def print_square(size):
+    """
+    Prints a square of '#' characters with the given size.
+    Args:
+        size (int): The size of the square.
+    """
+    if not isinstance(size, int):
+        raise TypeError("size must be an integer")
+    if size < 0:
+        raise ValueError("size must be >= 0")
+
+    for i in range(size):
+        print("#" * size)
