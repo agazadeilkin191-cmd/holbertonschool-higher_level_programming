a652bc4d773ca1ce13451b894c49a0c0c77c4e76
Ilkin Agazade <agazadeilkin191@email.com>
2026-04-24 15:04:15 +0000

    Add docstrings to module and function

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -1,5 +1,12 @@
 #!/usr/bin/python3
+"""
+3-say_my_name modulu
+"""
+
 def say_my_name(first_name, last_name=""):
+    """
+    first_name və last_name çap edən funksiya
+    """
     if not isinstance(first_name, str):
         raise TypeError("first_name must be a string")
     if not isinstance(last_name, str):
