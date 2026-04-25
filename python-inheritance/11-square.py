653c7dd99a757d155f3c39b812ed70c8987b0e9c
Ilkin Agazade <agazadeilkin191@email.com>
2026-04-25 13:04:15 +0000

    Fix: PEP8 line length in 11-square.py

--- python-inheritance/11-square.py
+++ python-inheritance/11-square.py
@@ -1,25 +1,19 @@
 #!/usr/bin/python3
 """
 Module 11-square
-Inherits from Rectangle and implements Square class with proper __str__.
+Inherits from Rectangle and implements Square class.
 """
 Rectangle = __import__('9-rectangle').Rectangle
 
 
 class Square(Rectangle):
-    """
-    Class Square that inherits from Rectangle.
-    """
+    """Class Square that inherits from Rectangle."""
     def __init__(self, size):
-        """
-        Initializes a Square instance with size.
-        """
+        """Initializes a Square instance."""
         self.integer_validator("size", size)
         self.__size = size
         super().__init__(size, size)
 
     def __str__(self):
-        """
-        Returns the string representation of the square: [Square] <width>/<height>
-        """
+        """Returns the string representation of the square."""
         return "[Square] {}/{}".format(self.__size, self.__size)
