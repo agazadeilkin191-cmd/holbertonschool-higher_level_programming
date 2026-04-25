a20d77d9f5ced1025fe4f9c637e23065f63a4523
Ilkin Agazade <agazadeilkin191@email.com>
2026-04-25 12:56:00 +0000

    Fix: PEP8 line length in 9-rectangle.py

--- python-inheritance/9-rectangle.py
+++ python-inheritance/9-rectangle.py
@@ -7,26 +7,20 @@ BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
-    """
-    Class Rectangle that inherits from BaseGeometry.
-    """
+    """Class Rectangle that inherits from BaseGeometry."""
     def __init__(self, width, height):
-        """
-        Initializes a Rectangle instance with width and height.
-        """
+        """Initializes a Rectangle instance."""
         self.integer_validator("width", width)
         self.integer_validator("height", height)
         self.__width = width
         self.__height = height
 
     def area(self):
-        """
-        Returns the area of the rectangle.
-        """
+        """Returns the area of the rectangle."""
         return self.__width * self.__height
 
     def __str__(self):
-        """
-        Returns the string representation of the rectangle: [Rectangle] <width>/<height>
+        """Returns the string representation of the rectangle:
+        [Rectangle] <width>/<height>
         """
         return "[Rectangle] {}/{}".format(self.__width, self.__height)
