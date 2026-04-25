9a3107c3ec992eae843da186254d6a4390711763
Ilkin Agazade <agazadeilkin191@email.com>
2026-04-25 13:10:15 +0000

    Task 13: Implement add_attribute function without try/except

---
+++ python-inheritance/101-add_attribute.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+Module 101-add_attribute
+Adds a new attribute to an object if possible.
+"""
+
+
+def add_attribute(obj, name, value):
+    """
+    Adds a new attribute to an object if it's possible.
+    Raises TypeError if it's not possible.
+    """
+    if not hasattr(obj, "__dict__"):
+        raise TypeError("can't add new attribute")
+    setattr(obj, name, value)
