a45b891acebee67b13cea0acdb4d4c76ebe6dbf2
Ilkin Agazade <agazadeilkin191@email.com>
2026-04-27 12:11:41 +0000

    Add 1-write_file.py

---
+++ python-input_output/1-write_file.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+Module 1-write_file
+Provides a function to write text to a file.
+"""
+
+
+def write_file(filename="", text=""):
+    """
+    Writes a string to a text file (UTF8) and returns
+    the number of characters written.
+    """
+    with open(filename, 'w', encoding='utf-8') as f:
+        return f.write(text)
