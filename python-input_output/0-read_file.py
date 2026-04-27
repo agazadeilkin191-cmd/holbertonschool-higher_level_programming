4501e38d058aab80a3e4faf5917bb51ff825a4b6
Ilkin Agazade <agazadeilkin191@email.com>
2026-04-27 12:06:37 +0000

    Fix: Add 0-read_file.py with docstrings and README.md

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,11 +1,16 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 """
-Task 0: Read file
+Module 0-read_file
+This module provides a function to read text files.
 """
+
 
 def read_file(filename=""):
     """
     Reads a text file (UTF-8) and prints it to stdout.
+
+    Args:
+        filename (str): The name of the file to read.
     """
     with open(filename, 'r', encoding='utf-8') as f:
         print(f.read(), end="")
