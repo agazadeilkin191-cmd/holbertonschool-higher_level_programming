746baf26f61349a6394a0450726d978a8332b70d
Ilkin Agazade <agazadeilkin191@email.com>
2026-04-29 12:25:02 +0000

    Add 11-student.py with reload_from_json

---
+++ python-input_output/11-student.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""
+11-student module
+"""
+
+
+class Student:
+    """
+    Student class
+    """
+    def __init__(self, first_name, last_name, age):
+        """
+        Initializes the Student instance
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """
+        Retrieves a dictionary representation of a Student instance
+        """
+        if isinstance(attrs, list):
+            return {
+                key: getattr(self, key)
+                for key in attrs
+                if hasattr(self, key)
+            }
+        return self.__dict__
+
+    def reload_from_json(self, json):
+        """
+        Replaces all attributes of the Student instance
+        """
+        for key, value in json.items():
+            setattr(self, key, value)
