02c2b39eb9d1b926e20f369cdf610410110e31aa
Ilkin Agazade <agazadeilkin191@email.com>
2026-05-11 18:07:06 +0000

    List by best score

---
+++ SQL_introduction/10-top_score.sql
@@ -0,0 +1,4 @@
+-- lists all records of the table second_table
+-- results should display score and name (in this order)
+-- records should be ordered by score (top first)
+SELECT score, name FROM second_table ORDER BY score DESC;
