c3d94f5e76757adc7579bc3c1741a83165423c9c
Ilkin Agazade <agazadeilkin191@email.com>
2026-05-16 07:37:43 +0000

    Fix indentation once and for all using echo command

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -1,18 +1,12 @@
 #!/usr/bin/python3
-"""
-This module defines a State class and an instance Base = declarative_base().
-"""
+"""This module defines a State class."""
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 
-
 class State(Base):
-    """
-    State class links to the MySQL table states.
-    """
-    __tablename__ = 'states'
-
+    """State class links to the MySQL table states."""
+    __tablename__ = "states"
     id = Column(Integer, primary key=True, nullable=False, unique=True)
     name = Column(String(128), nullable=False)
