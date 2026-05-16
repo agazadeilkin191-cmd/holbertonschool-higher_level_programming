cdc92ae20c23dbd162941b5ff643d5e2a20601c6
Ilkin Agazade <agazadeilkin191@email.com>
2026-05-16 06:49:59 +0000

    Fix PEP8 line length issue

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -15,7 +15,10 @@ if __name__ == "__main__":
         db=sys.argv[3]
     )
     cursor = db.cursor()
-    query = "SELECT * FROM states WHERE name LIKE BINARY '{}' ORDER BY id ASC".format(sys.argv[4])
+    query = (
+        "SELECT * FROM states WHERE name LIKE BINARY '{}' "
+        "ORDER BY id ASC".format(sys.argv[4])
+    )
     cursor.execute(query)
     rows = cursor.fetchall()
     for row in rows:
