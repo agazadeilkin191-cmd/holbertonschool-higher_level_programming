1132a3c86518de419892c5bc2833b8bbfe8ad62d
Ilkin Agazade <agazadeilkin191@email.com>
2026-05-16 06:57:41 +0000

    Add 5-filter_cities.py with parameterized JOIN query

---
+++ python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""
+This module provides a script that takes in the name of a state
+as an argument and lists all cities of that state from the database.
+"""
+import sys
+import MySQLdb
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3]
+    )
+    cursor = db.cursor()
+    query = (
+        "SELECT cities.id, cities.name, states.name "
+        "FROM cities "
+        "JOIN states ON cities.state_id = states.id "
+        "WHERE states.name LIKE BINARY %s "
+        "ORDER BY cities.id ASC"
+    )
+    cursor.execute(query, (sys.argv[4],))
+    rows = cursor.fetchall()
+    for row in rows:
+        print(row)
+    cursor.close()
+    db.close()
