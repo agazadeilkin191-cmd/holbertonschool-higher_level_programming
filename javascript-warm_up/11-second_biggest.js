3a8df589081aaefd4aaa2020c908c947e966d02f
Ilkin Agazade <agazadeilkin191@email.com>
2026-06-05 13:12:12 +0000

    Fix trailing spaces in 11-second_biggest.js

--- javascript-warm_up/11-second_biggest.js
+++ javascript-warm_up/11-second_biggest.js
@@ -5,7 +5,5 @@ if (args.length <= 1) {
   console.log(0);
 } else {
   const sorted = args.sort((a, b) => b - a);
-  // Unikal dəyərlər üçün Set istifadə etmək olar, 
-  // lakin tapşırıq sadə sıralama tələb edir
   console.log(sorted[1]);
 }
